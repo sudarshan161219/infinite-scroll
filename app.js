b67a787aaf7f704b0c195e4fb2056376ccf9f267
Sudarshan Hosalli@DESKTOP-F068LQM <godhustler90956@gmail.com>
2023-02-19 08:01:05 +0530

    update

--- app.js
+++ app.js
@@ -1,42 +1,52 @@
 const imageContainer = document.getElementById('image-container')
 const loader = document.getElementById('loader')
 
+let ready = false
+let imagesLoaded = 0
+let totalImages = 0
 let photosArray = [];
 
 // unsplash api
-const count = 10
+const count = 30
 const apiKey = 'yobLbQ0Q0C8hv4lGGdDoZKJ_7fNVe0Eulk9rRtPBv48'
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`
 
-function setAttributes (element, attributes){
+function setAttributes(element, attributes) {
     for (const key in attributes) {
         element.setAttribute(key, attributes[key])
     }
 }
 
+function imageLoaded() {
+    imagesLoaded++
+    if(imagesLoaded === totalImages) {
+        ready = true
+        loader.hidden = true
+    }
+}
+
 //Create Element for Links and Photos 
 function displayPhotos() {
+    imagesLoaded = 0
+    totalImages = photosArray.length
     //Run Function for each object in photoArray
     photosArray.forEach((photo) => {
         // Create <a></a> to link unsplash
         const item = document.createElement('a')
-        // item.setAttribute('href', photo.links.html)
-        // item.setAttribute('target', '_blank')
-        setAttributes (item,{
-            href:  photo.links.html,
+        setAttributes(item, {
+            href: photo.links.html,
             target: '_blank'
         })
 
         //Create image for photo
         const img = document.createElement('img')
-        // img.setAttribute('src', photo.urls.regular)
-        // img.setAttribute('alt', photo.alt_description)
-        // img.setAttribute('title', photo.alt_description)
-setAttributes(img, {
-    src: photo.urls.regular,
-    alt: photo.alt_description,
-    title: photo.alt_description,
-})
+        setAttributes(img, {
+            src: photo.urls.regular,
+            alt: photo.alt_description,
+            title: photo.alt_description,
+        })
+
+        img.addEventListener('load', imageLoaded)
 
         // put <img> inside  <a>
         item.appendChild(img)
@@ -51,7 +61,7 @@ async function getPhotos() {
         const response = await fetch(apiUrl)
         photosArray = await response.json()
         // console.log(photosArray);
-        // displayPhotos()
+        displayPhotos()
 
     } catch (error) {
         console.log(error);
@@ -59,9 +69,10 @@ async function getPhotos() {
 }
 
 window.addEventListener('scroll', () => {
-   if(window.innerHeight + window.screenY >= document.body.offsetHeight - 1000){
-    
-   }
+    if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000 && ready)  {
+        ready = false
+        getPhotos()
+    }
 })
 
-// getPhotos()
+getPhotos()
